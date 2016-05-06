e90d704a3cf8d014d313311d68764b46367c8026
keynescao <keynescao@163.com>
2016-05-06 13:41:14 +0800

    add netty demo test

---
+++ src/com/demo/netty/server/handler/DiscardServerHandler.java
@@ -0,0 +1,17 @@
+package com.demo.netty.server.handler;
+
+import io.netty.channel.ChannelHandlerAdapter;
+
+/**
+ * @author keynes
+ * server handler 
+ */
+public class DiscardServerHandler extends ChannelHandlerAdapter {
+
+	
+
+	
+
+	
+	
+}
