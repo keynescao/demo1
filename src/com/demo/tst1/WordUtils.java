4e3b158ab0629c7b396cdb54ba1935a330892510
keynescao <keynescao@163.com>
2016-05-26 17:19:17 +0800

    test

--- src/com/demo/tst1/WordUtils.java
+++ src/com/demo/tst1/WordUtils.java
@@ -147,7 +147,7 @@ public class WordUtils {
         	System.out.println("File " + input + " not exist");
         	return;
         }
-        File outputFile = new File(output); 
+        File outputFile = new File(output);
         
         String command = "/opt/openoffice4/"   
                 + "program/soffice -headless -accept=\"socket,host=127.0.0.1,port=8100;urp;\"";   
